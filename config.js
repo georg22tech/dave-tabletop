f1f656684a59ba6626f2dff03056970f963e82a6
georg22tech <158208127+georg22tech@users.noreply.github.com>
2026-01-15 15:47:32 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBO7B4K_VugP0ci2sS58mikWB_tWaZ6uGo",
+  authDomain: "dndave-3166e.firebaseapp.com",
+  projectId: "dndave-3166e",
+  storageBucket: "dndave-3166e.firebasestorage.app",
+  messagingSenderId: "135042217085",
+  appId: "1:135042217085:web:fe045c9edcfe1c5c2bed14"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
