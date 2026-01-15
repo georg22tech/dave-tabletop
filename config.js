21a3a34e30546789be73a86c9ce7b687df109ccc
georg22tech <158208127+georg22tech@users.noreply.github.com>
2026-01-15 15:55:58 +0000

    Update Firebase config to include databaseURL
    Added databaseURL to Firebase configuration.

--- config.js
+++ config.js
@@ -4,10 +4,12 @@ const firebaseConfig = {
   apiKey: "AIzaSyBO7B4K_VugP0ci2sS58mikWB_tWaZ6uGo",
   authDomain: "dndave-3166e.firebaseapp.com",
   projectId: "dndave-3166e",
+  databaseURL: "https://dndave-3166e-default-rtdb.firebaseio.com",
   storageBucket: "dndave-3166e.firebasestorage.app",
   messagingSenderId: "135042217085",
   appId: "1:135042217085:web:fe045c9edcfe1c5c2bed14"
 };
 
 firebase.initializeApp(firebaseConfig);
+
 const db = firebase.database();
