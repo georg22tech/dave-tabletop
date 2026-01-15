e98db90a948d0727611718cecd86a13a3b4d4d34
georg22tech <158208127+georg22tech@users.noreply.github.com>
2026-01-15 15:58:02 +0000

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBO7B4K_VugP0ci2sS58mikWB_tWaZ6uGo",
   authDomain: "dndave-3166e.firebaseapp.com",
   projectId: "dndave-3166e",
-  databaseURL: "https://dndave-3166e-default-rtdb.firebaseio.com",
+  databaseURL: "https://dndave-3166e-default-rtdb.europe-west1.firebasedatabase.app/",
   storageBucket: "dndave-3166e.firebasestorage.app",
   messagingSenderId: "135042217085",
   appId: "1:135042217085:web:fe045c9edcfe1c5c2bed14"
@@ -13,3 +13,4 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const db = firebase.database();
+
